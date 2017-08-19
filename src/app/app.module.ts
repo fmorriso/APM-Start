316a174dd6073b5ea1e6623b7c47c476ff061f71
Fred Morrison <fpmorrison@verizon.net>
2017-08-19 14:45:55 -0400

    added missing import and declaration of WelcomeComponent that are exposed during --prod build or start.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 // The following import and corresponding declaration
-// are missing from Ddeborah Kruata's August 2017 edition of APM-Start
+// are missing from Deborah Kruata's August 2017 edition of APM-Start
 // Nothing bad happens until you start the application using:
 // ng serve --open --verbose --vendor-chunk --extract-css --prod
 import { WelcomeComponent} from './home/welcome.component';
