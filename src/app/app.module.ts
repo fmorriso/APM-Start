b68c09bbd72a8e3e42d90d7feb731b7b36828bb4
Fred Morrison <fpmorrison@verizon.net>
2017-08-20 16:53:19 -0400

    Clean up use of Boostrap classes in the HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 // The following import and corresponding declaration
-// are missing from Deborah Kruata's August 2017 edition of APM-Start
+// are missing from Deborah Kurata's August 2017 edition of APM-Start
 // Nothing bad happens until you start the application using:
 // ng serve --open --verbose --vendor-chunk --extract-css --prod
-import { WelcomeComponent} from './home/welcome.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
